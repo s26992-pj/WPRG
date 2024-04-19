688190b1c93e43b51f7e7418053d76c64370ec8b
s26992-pj <116387081+s26992-pj@users.noreply.github.com>
2024-04-20 00:50:47 +0200

    Update zad3.php

--- zjazd2/zad3.php
+++ zjazd2/zad3.php
@@ -6,34 +6,21 @@
 </head>
 <body style="text-align: center">
 <form method="post" action="">
-  <label for="ilosc">Ilość ludzi: </label>
-  <br>
+  <label for="ilosc">Ilość ludzi: </label><br>
   <select name="ilosc" id="ilosc">
     <option value="1">1</option>
     <option value="2">2</option>
     <option value="3">3</option>
     <option value="4">4</option>
-  </select>
-  <br>
-
-  <?php
-  if (isset($_POST['ilosc'])) {
-      $ilosc_osob = intval($_POST['ilosc']);
-      for ($i = 1; $i <= $ilosc_osob; $i++) {
-          echo "<p>Osoba $i:</p>";
-  echo "<input type='text' name='imie$i' placeholder='Imię osoby $i'><br>";
-  echo "<input type='text' name='nazwisko$i' placeholder='Nazwisko osoby $i'><br>";
-  echo "<input type='text' name='adres$i' placeholder='Adres osoby $i'><br>";
-  echo "<br>";
-  }
-  }
-  ?>
-
-  <input type="number" name="blik" id="blik" maxlength="6" placeholder="Blik" pattern="[0-9]{6}"><br>
-  <label for="data przyjazdu">Data przyjazdu:</label><br>
-  <input type="date" name="data przyjazdu" id="data przyjazdu"><br>
-  <label for="data wyjazdu">Data wyjazdu:</label><br>
-  <input type="date" name="data wyjazdu" id="data wyjazdu"><br>
+  </select><br>
+  <input type="text" name="imie" id="imie" placeholder="Imię"><br>
+  <input type="text" name="nazwisko" id="nazwisko" placeholder="Nazwisko"><br>
+  <input type="text" name="adres" id="adres" placeholder="Adres"><br>
+  <input type="text" name="blik" id="blik" maxlength="6" placeholder="Blik" pattern="[0-9]{6}"><br>
+  <label for="data_przyjazdu">Data przyjazdu:</label><br>
+  <input type="date" name="data_przyjazdu" id="data_przyjazdu"><br>
+  <label for="data_wyjazdu">Data wyjazdu:</label><br>
+  <input type="date" name="data_wyjazdu" id="data_wyjazdu"><br>
   <input type="checkbox" id="popielniczka" name="popielniczka">
   <label for="popielniczka">Popielniczka: </label><br>
   <input type="checkbox" id="klimatyzacja" name="klimatyzacja">
@@ -41,37 +28,33 @@
   <input type="checkbox" id="lozko" name="lozko">
   <label for="lozko">Łóżko dla dziecka</label><br><br>
 
-  <button type="submit">Zarezerwuj</button><br>
+  <button type="submit" name="submit">Zarezerwuj</button><br>
 </form>
 <div class="book">
   <?php
-  if ($_POST) {
-      $ilosc = $_POST['ilosc'];
-      $blik = $_POST['przelew'];
-      $data_przyjazdu = $_POST['data_przyjazdu'];
-      $data_wyjazdu = $_POST['data_wyjazdu'];
-      $popielniczka = isset($_POST['popielniczka']) ? 'Tak' : 'Nie';
-      $klimatyzacja = isset($_POST['klimatyzacja']) ? 'Tak' : 'Nie';
-      $lozko = isset($_POST['lozko']) ? 'Tak' : 'Nie';
+    if (isset($_POST['submit'])){
+        $ilosc = $_POST['ilosc'];
+        $imie = $_POST['imie'];
+        $nazwisko = $_POST['nazwisko'];
+        $adres = $_POST['adres'];
+        $blik = $_POST['blik'];
+        $data_przyjazdu = $_POST['data_przyjazdu'];
+        $data_wyjazdu = $_POST['data_wyjazdu'];
+        $popielniczka = isset($_POST['popielniczka']) ? 'Tak' : 'Nie';
+        $klimatyzacja = isset($_POST['klimatyzacja']) ? 'Tak' : 'Nie';
+        $lozko = isset($_POST['lozko']) ? 'Tak' : 'Nie';
 
-  echo "<p>Ilość ludzi: $ilosc</p>";
-
-  for ($i = 1; $i <= $ilosc; $i++) {
-    $imie = $_POST["imie$i"];
-    $nazwisko = $_POST["nazwisko$i"];
-    $adres = $_POST["adres$i"];
-    echo "<h3>Dane osoby $i:</h3>";
-    echo "<p>Imię: $imie</p>";
-    echo "<p>Nazwisko: $nazwisko</p>";
-  }
-  echo "<p>Adres: $adres</p>";
-  echo "<p>Transakcja blik zatwierdzona</p>";
-  echo "<p>Data przyjazdu: $data_przyjazdu</p>";
-  echo "<p>Data wyjazdu: $data_wyjazdu</p>";
-  echo "<p>Czy popielniczka: $popielniczka</p>";
-  echo "<p>Czy klimatyzacja: $klimatyzacja</p>";
-  echo "<p>Czy łóżko dla dziecka: $lozko</p>";
-  }
+        echo "<p>Ilość ludzi: $ilosc</p>";
+        echo "<p>Imię: $imie</p>";
+        echo "<p>Nazwisko: $nazwisko</p>";
+        echo "<p>Adres: $adres</p>";
+        echo "<p>Transakcja blik zatwierdzona</p>";
+        echo "<p>Data przyjazdu: $data_przyjazdu</p>";
+        echo "<p>Data wyjazdu: $data_wyjazdu</p>";
+        echo "<p>Czy popielniczka: $popielniczka</p>";
+        echo "<p>Czy klimatyzacja: $klimatyzacja</p>";
+        echo "<p>Czy łóżko dla dziecka: $lozko</p>";
+    }
   ?>
 </div>
 </body>
